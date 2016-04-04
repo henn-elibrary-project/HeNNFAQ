8908ddb27d652888e53554ed1e36ea69f4161c3a
algosig <sigsandy02@gmail.com>
2016-04-04 08:31:17 +0545

    Created file:admin_panel.php
    where administrator will add the questions and monitors the request of questions

---
+++ admin_panel.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title> Henl Nepal Network | E-Library PROJECT | One E-LIBRARY PER DISTRICT </title>
+		<link type="text/css" rel="stylesheet" href="style/style.css"></link>
+		<link rel="shortcut icon" style="border-color:red;" href="images/logo.png"  type="image/x-icon">
+	</head>
+	<body>
+		<div class='wrapper'>
+				<?php include'menubar.php';?>
+
+			<div id="marquee">
+				<marquee direction="left" style="background-color:transparent; text-decoration:underline;"><?php include'marquee.php' ?></marquee>
+			</div>
+
+			<?php include'admin_menubar.php';?>			
+
+			<div class="left">
+						TEST LEFT DIVISION
+			</div>
+			
+			<div class="right">
+						TEST RIGHT DIVISION
+			</div>
+
+		</div>
+	</body>
+</html>
