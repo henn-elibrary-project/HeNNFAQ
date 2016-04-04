094a24533b7c936c3383e1533a9ce60cf9422dca
algosig <sigsandy02@gmail.com>
2016-04-04 08:30:03 +0545

    Created new file: admin_menubar.php
    menubar for administrator

---
+++ admin_menubar.php
@@ -0,0 +1,12 @@
+<div id='men_wrapper'>
+	<div id='men'>
+		<ul>
+		<li><a href='view_report.php'>REPORT</a></li>
+		<li><a href='view_feedback.php'>FEEDBACK</a></li>
+		<li><a href='faq.php'>ADD FAQ</a></li>
+		<li><a href='volunteers.php'>VOLUNTEERS</a></li>
+		<li><a href="logout.php">logout  <style='text-color:red;'></a></li>
+		</ul>
+	</div>
+
+</div>
