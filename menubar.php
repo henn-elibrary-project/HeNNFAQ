d11adc1c8a294a7069105551181a1bcdfd51f4f6
algosig <sigsandy02@gmail.com>
2016-04-04 08:40:31 +0545

    created file: menubar.php
    Displaying the top panel and menubar of the website

---
+++ menubar.php
@@ -0,0 +1,32 @@
+<div id='menu_wrapper'>
+	
+	<div id='logo'>
+		<img src="images/logo.png" tab="HELP NEPAL NETWORK" height="96" width="67"> 
+	</div>
+	
+	<div id='title'>
+	<h style="font-size:35px;background-color:transparent;">HELP NEPAL NETWORK</h>
+	<br>
+	<h style="font-size:25px;background-color:transparent; color:#333;"> E-LIBRARY PROJECT </h><br>One E-Library Per District
+	</div>
+	
+	<div id='menu'>
+		<ul>
+		<li><a href='index.php'>HOME</a></li>
+<!-- ##In case to use the drop down menu but not the final touch. Still rough##
+<select style="background-color:transparent; border-radius:0px;">
+<option value="HeNN"><a href='http://www.helpnepal.net'>HeNN</a></option>
+<option value="saab">Saab</option>
+<option value="opel">Opel</option>
+<option value="audi">Audi</option>
+</select>
+-->		<li><a href='http://www.helpnepal.net'>HeNN</a></li>
+		<li><a href='report.php'>REPORT</a></li>
+		<li><a href='view_faq.php'>FAQ</a></li>
+		<li><a href='feedback.php'>FEEDBACK</a></li>
+		<li><a href='volunteers.php'>VOLUNTEERS</a></li>
+		<li><a href='login.php'>ADMIN</a></li>
+		</ul>
+	</div>
+
+</div>
